4509f8cf6076d40e7aa7c6a545e7ee432b16fdff
vasile luta <elsile69@yahoo.com>
2020-09-22 13:25:48 +0300

    refactor: with useState and useEffect

--- src/WebcomponentCowntdown.js
+++ src/WebcomponentCowntdown.js
@@ -1,16 +1,24 @@
-import { component, html } from 'haunted';
+import {component, html, useState, useEffect} from 'haunted';
 
-function Countdown({ to }) {
-  // const now = +new Date();
-  // const seconds = () => ({to} * 1000);
+function Countdown({to}) {
+  const [seconds, setSeconds] = useState(0);
 
-  return html`
-    <h1>${to}</h1>
-    <div></div>
-  `;
+  useEffect(() => {
+    const updateTime = () => {
+      const now = Math.round(+new Date() / 1000);
+      const toInSeconds = Math.round(+new Date(to) / 1000);
+      setSeconds(toInSeconds - now);
+    };
+
+    updateTime();
+    const i = setInterval(updateTime, 1000);
+    return () => clearInterval(i);
+  }, [to]);
+
+  return html`<div>${seconds} sec</div>`;
 }
 
 customElements.define(
   'webcomponent-countdown',
-  component(Countdown, { observedAttributes: ['to'] })
+  component(Countdown, {observedAttributes: ['to']})
 );
