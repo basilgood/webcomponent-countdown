0819559111dfa28209b7c3c9848663cb126e2c0a
vasile luta <elsile69@yahoo.com>
2020-09-28 12:31:07 +0300

    refactor: refactor code

--- src/WebcomponentCowntdown.js
+++ src/WebcomponentCowntdown.js
@@ -1,30 +1,49 @@
-import {component, html, useState, useEffect} from 'haunted';
+import { component, html, useEffect, useState } from 'haunted';
 
-function Countdown({to}) {
-  const toevent = Date.parse(to) - Date.parse(new Date());
-  // const [seconds, setSeconds] = useState(0);
-  //
-  // useEffect(() => {
-  //   const updateTime = () => {
-  //     const now = Math.round(+new Date() / 1000);
-  //     const toInSeconds = Math.round(+new Date(to) / 1000);
-  //     setSeconds(toInSeconds - now);
-  //   };
-  //
-  //   updateTime();
-  //   const i = setInterval(updateTime, 1000);
-  //   return () => clearInterval(i);
-  // }, [to]);
+function Countdown({ to }) {
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const i = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(i);
+  }, []);
+
+  const toevent = Date.parse(to) - Date.parse(now);
 
   return html`
     <h3>${to}</h3>
-    <div>Days ${Math.floor(toevent/(1000*60*60*24))}</div>
-    <div>Hours ${Math.floor((toevent/(1000*60*60)) % 24)}</div>
-    <div>Minutes ${Math.floor((toevent/(1000*60)) % 60)}</div>
+    <div>Days ${Math.floor(toevent / (1000 * 60 * 60 * 24))}</div>
+    <div>Hours ${Math.floor((toevent / (1000 * 60 * 60)) % 24)}</div>
+    <div>Minutes ${Math.floor((toevent / (1000 * 60)) % 60)}</div>
+    <div>Seconds ${Math.floor((toevent / 1000) % 60)}</div>
   `;
 }
 
 customElements.define(
   'webcomponent-countdown',
-  component(Countdown, {observedAttributes: ['to']})
+  component(Countdown, { observedAttributes: ['to'] })
 );
+
+function App() {
+  const [x, setX] = useState('2021-01-01');
+
+  // Un exemplu de a folosi useEffect:
+  // cand se schimba x, iti scrie valoarea in consola
+  // useEffect(() => console.log(x), [x]);
+  useEffect(() => {
+    const today = new Date(x);
+    if (today.getDate() === 1 && today.getMonth() === 0) {
+      console.log('Happy New Year, ' + today.getFullYear());
+    }
+  }, [x]);
+
+  // sa folosesti useEffect, ca atunci cand userul alege prima zi dintr-un an
+  // sa ii scrie in consola, "La multi ani, 2025!"
+
+  return html`
+    <input type="date" @input=${ev => setX(ev.target.value)} />
+    <webcomponent-countdown to=${x}></webcomponent-countdown>
+  `;
+}
+
+customElements.define('my-app', component(App));
